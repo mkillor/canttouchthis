8669be072f413f1949e8fc2ea4d42e9e6229ff6a
mkillor <mark.killoran.jr@gmail.com>
2022-12-11 03:04:39 +0000

    asfsfsdaf

--- src/com/company/killoran/Main.java
+++ src/com/company/killoran/Main.java
@@ -1,8 +1,16 @@
 package com.company.killoran;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hammertime!");
+        System.out.println("does this work?");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type your name here:");
+        String name = input.nextLine();
+        System.out.println("Hello" + name);
     }
 }
